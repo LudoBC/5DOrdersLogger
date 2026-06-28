6e297d4f56605c0b51407b4254c3f0ed35943c4d
LudoBC <ludobvw@gmail.com>
2026-06-28 23:05:10 +0200

    ensure that exampleInput is deleted when running mation coverage

--- src/test/java/nodomain/mijnmooiewereld/orders/logger/acceptance/LocalFileTest.java
+++ src/test/java/nodomain/mijnmooiewereld/orders/logger/acceptance/LocalFileTest.java
@@ -1,23 +1,29 @@
 package nodomain.mijnmooiewereld.orders.logger.acceptance;
 
 import nodomain.mijnmooiewereld.orders.logger.Main;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LocalFileTest {
+    static final Path
+            exampleInputMD = Path.of("exampleInput.md"),
+            exampleOutputMd = Path.of("exampleOutput.md");
+
     @Test
     void testLogWriterFromLocalFile() throws IOException {
-        Path exampleInputMD = Path.of("exampleInput.md"), exampleOutputMd = Path.of("exampleOutput.md");
         Files.deleteIfExists(exampleInputMD);
         Main.main("exampleInput.json");
         assertTrue(FileComparator.haveSameContent(exampleInputMD, exampleOutputMd));
-        Files.delete(exampleInputMD);
-        assertFalse(Files.exists(exampleInputMD));
+    }
+
+    @AfterAll
+    static void deleteCreatedFile() throws IOException {
+        Files.deleteIfExists(exampleInputMD);
     }
 }
